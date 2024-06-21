06352da5189400471e91eb72c5cdd3807433343a
Bao Tran <baopei05@gmail.com>
2024-06-21 14:39:05 -0300

    change from hex to base91 for encryption and decrytion

--- src/app/actions/Fucntion/encrypt-decrypt.ts
+++ src/app/actions/Fucntion/encrypt-decrypt.ts
@@ -1,6 +1,7 @@
 'use server'
 import crypto from 'crypto'
 
+const base91 = require('node-base91')
 export const EncryptDecrypt = async ({
   key,
   text,
@@ -60,15 +61,17 @@ const encrypt = ({ text, key }: { text: string; key: Buffer }) => {
 
   const cipher: crypto.Cipher = crypto.createCipheriv('aes-256-cbc', key, iv)
 
-  let encrypted: string = cipher.update(text, 'utf-8', 'hex')
-  encrypted += cipher.final('hex')
-  return iv.toString('hex') + ':' + encrypted
+  let encrypted: string = cipher.update(text, 'utf-8', 'binary')
+  encrypted += cipher.final('binary')
+
+  const encryptedData = iv.toString('binary') + encrypted // convert iv to string and add it to the front
+  return base91.encode(Buffer.from(encryptedData, 'binary')) // then use base91 to convert it to string
 }
 
 const decrypt = ({ text, key }: { text: string; key: Buffer }) => {
-  const parts: string[] = text.split(':')
-  const iv: Buffer = Buffer.from(parts.shift() as string, 'hex')
-  const cipherText: string = parts.join(':')
+  const encryptedData = base91.decode(text);
+  const iv: Buffer = encryptedData.slice(0, 16);
+  const encrypted: string = encryptedData.slice(16)
 
   const decipher: crypto.Cipher = crypto.createDecipheriv(
     'aes-256-cbc',
@@ -76,7 +79,7 @@ const decrypt = ({ text, key }: { text: string; key: Buffer }) => {
     iv
   )
 
-  let decrypted: string = decipher.update(cipherText, 'hex', 'utf8')
+  let decrypted: string = decipher.update(encrypted, 'hex', 'utf8')
   decrypted += decipher.final('utf8')
   return decrypted
 }
